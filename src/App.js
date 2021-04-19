b0d5172ac3a57a420b05f1838399424514cf98e7
Abel Castillo <abcalajer@gmail.com>
2021-04-18 21:10:09 -0500

    Se agrega Amplify (API Graphql y Autenticacion)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./tailwind.output.css";
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
 
 function App() {
   return (
     <div className="bg-red-500 txt-2xl">
+      <AmplifySignOut />
       Hola Mundo
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
